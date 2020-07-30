c3543599d94c8aba2c91597dec37508bae4f47ff
Arun.R.R <arun90rr@gmail.com>
2020-07-30 21:21:56 +0530

    :clown_face: Add: dummy data
    Added dummy data for redux store

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import startServer from "./server";
 
 // Create Redux store
 const store = makeStore();
+// Start socket io server
 startServer(store);
+
+// Initialize state with dummy entries data
+store.dispatch({
+  type: "SET_ENTRIES",
+  entries: require("./entries.json"),
+});
+// Select first pair of entries to vote
+store.dispatch({
+  type: "NEXT",
+});
